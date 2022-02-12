5a06ed81e2caf13d3c7ac48795042a572b9e0e2c
ankitkumar1998 <77103658+ankitkumar1998@users.noreply.github.com>
2022-02-12 18:18:40 +0530

    second commit

--- Git-Jam/src/main/java/javajam/Git_Jam/App.java
+++ Git-Jam/src/main/java/javajam/Git_Jam/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
+    	int i=10;
+    	int j=20;
+    	int z=i+j;
+    	System.out.println(z);
         System.out.println( "Hello World!" );
     }
 }
